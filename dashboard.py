a4a03c0ff12d0c59d1215f59f008d82fc86c8622
mars28222-spec <mars28222@gmail.com>
2025-10-18 10:46:17 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -11,7 +11,7 @@ import cv2
 # ==========================
 @st.cache_resource
 def load_models():
-    yolo_model = YOLO("D:\Praktikum S5\Big Data\Siti Marlina_Laporan 4.pt")  # Model deteksi objek
+    yolo_model = YOLO(r"D:\Praktikum S5\Big Data\Siti Marlina_Laporan 4.pt")  # Model deteksi objek
     classifier = tf.keras.models.load_model("D:\Praktikum S5\Big Data\Siti Marlina_Laporan 2.h5")  # Model klasifikasi
     return yolo_model, classifier
 
